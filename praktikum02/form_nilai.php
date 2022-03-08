ddf5f2fc89d791367986a3ab4e0190ef467dc013
Satria Ramadhan <91797403+satriar14@users.noreply.github.com>
2022-03-08 18:26:56 +0700

    Update form_nilai.php

--- praktikum02/form_nilai.php
+++ praktikum02/form_nilai.php
@@ -6,7 +6,7 @@
     <title>Form</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./style2.css">
   </head>
   <body>
     <div class="container">
